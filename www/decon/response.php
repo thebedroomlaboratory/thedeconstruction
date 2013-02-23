3d7deb0fb16073f7e27973f851ebcd506631e51a
root <root@raspberrypi.(none)>
2013-02-23 21:49:09 +0000

    Resonse.php now communicating with local server

--- www/decon/response.php
+++ www/decon/response.php
@@ -1,14 +1,28 @@
 <?php
 $xml = simplexml_load_file("test.xml");
-
-echo "<div id='name'>" . $xml->getName() . "<br> </div>";
-
+$con = mysql_connect("localhost","root","root");
+if(!$con){
+  //echo "<div id='name'> Title <br> </div>";
+  //echo "<div id='temp'> Connection Open <br> </div>";
+  die('Could not connect: ' . mysql_error());
+}
+//echo "<div id='name'>" . $xml->getName() . "<br> </div>";
 //foreach($xml->children() as $child)
-  //{
-  //echo  $child->getName() . ": " . $child . "&#176;<br>";
-  //}
-  
-  echo "<div id='temp'>" . $xml->Temp . "&#176;<br> </div>";
+//{
+//echo  $child->getName() . ": " . $child . "&#176;<br>";
+//}
+//echo "<div id='temp'>" . $xml->Temp . "&#176;<br> </div>";
+//echo "<div id='temp'> Its a celebration bitches! &#176;<br> </div>";
+
+mysql_select_db("tbl_thermostat", $con);
+
+$result = mysql_query("SELECT * FROM tbl_room_info");
+while($row = mysql_fetch_array($result)){
+  echo "<div id='name" . $row['unique_id'] . "'> Room " . $row['device_id'] . "<br> </div>";
+  echo "<div id='temp" . $row['unique_id'] . "'> " . $row['temperature'] . "<br> </div>";
+}
+
+mysql_close($con);
 ?> 
 
 
